e1a8fa989eb0129417057c90694f6f34000fb062
Mohammed Zakria Khan <mohammedzakriakhanz@gmai.com>
2023-12-21 12:15:03 +0530

    craeted 3 microservice and authentication added

---
+++ Mango/Mango.Web/Utility/SD.cs
@@ -0,0 +1,22 @@
+﻿namespace Mango.Web.Utility
+{
+    public class SD
+    {
+        public static string CouponAPIBase { get; set; }
+        public static string AuthApiBase { get; set; }
+
+        public static string RoleAdmin = "ADMIN";
+
+        public static string RoleCustomer = "CUSTOMER";
+
+        public static string TokenCookie = "JWTToken";
+
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
